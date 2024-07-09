43e6f8445b0651b4ef5901f8f20c2eeb962f4014
Al-amen <alamen15@cse.pstu.ac.bd>
2024-07-09 12:24:32 +0600

    added str function

--- Blog_Project/App_Login/models.py
+++ Blog_Project/App_Login/models.py
@@ -9,4 +9,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, related_name='user_profile', on_delete=models.CASCADE)
     profile_pic = models.ImageField(upload_to='profile_pics')
     
+    def __str__(self):
+        return self.user.username
+    
 
