3430b07ea3e21863a1b8cdaf8d748d11cf813284
Al-amen <alamen15@cse.pstu.ac.bd>
2024-07-09 12:45:22 +0600

    register Blog , Comment and Likes models are registered to admin panel

--- Blog_Project/App_Blog/admin.py
+++ Blog_Project/App_Blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from App_Blog import models
 # Register your models here.
+
+admin.site.register(models.Blog)
+admin.site.register(models.Comment)
+admin.site.register(models.Likes)
