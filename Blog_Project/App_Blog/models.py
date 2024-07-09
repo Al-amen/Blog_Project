e026be901b5885d9b850fa914a6b9a3c99a04c17
Al-amen <alamen15@cse.pstu.ac.bd>
2024-07-09 12:21:43 +0600

    create Blog Comment and Likes models

--- Blog_Project/App_Blog/models.py
+++ Blog_Project/App_Blog/models.py
@@ -1,4 +1,33 @@
 from django.db import models
 
 from django.contrib.auth.models import User
-# Create your models here.
+
+
+class Blog(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_blog')
+    blog_title = models.CharField(max_length= 250, verbose_name='Put a title')
+    slug = models.SlugField(max_length=250,unique=True)
+    blog_content = models.CharField(max_length=300,verbose_name="What is on your mind ?")
+    blog_image = models.ImageField(upload_to='blog_images', verbose_name="Image")
+    publish_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return self.blog_title
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="blog_comment")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_user")
+    comment = models.TextField()
+    comment_date = models.DateField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.comment
+
+
+class Likes(models.Model):
+     blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="liked_blog") 
+     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="liked_user")
+     
+     
+    
